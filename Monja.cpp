5b282823b9963fa71162caba5981ca135d6f6d29
JDRV17 <123598113+JDRV17@users.noreply.github.com>
2024-02-14 12:56:31 -0500

    Update Monja.cpp

--- Monja.cpp
+++ Monja.cpp
@@ -1,20 +1,26 @@
 #include "Monja.h"
 
-void Monja::DibujarMonja(float x, float y, float z) 
+void Monja::DibujarMonja(int seg, float alpha, float x, float y, float z) 
 {
-    glPushMatrix();
-        glPushMatrix();
-            glTranslatef(x, y - 0.1, z);
-            glutSolidSphere(0.5, 20, 20);
-        glPopMatrix();
-
-        glPushMatrix();
-            glTranslatef(x, y, z);
-            glBegin(GL_TRIANGLES);
-                glVertex3f(1, 0, 0);
-                glVertex3f(0, 1, 0);
-                glVertex3f(-1, 0, 0);
-            glEnd();
-        glPopMatrix();
-     glPopMatrix();
+	glPushMatrix();
+		glTranslatef(x-1, y+0.0, z+0.0);
+		glRotatef((GLfloat)alpha, 0.0, 0.0, 1.0);
+		glTranslatef(x-1, y+0.0, z+0.0);
+		glPushMatrix();
+			glScalef(2, 0.4, 1.0);
+			glutSolidCube(1.0); /* shoulder */
+		glPopMatrix();
+		
+		for (int i = 0; i < seg; i++)
+		{
+			glTranslatef(1, 0.0, 0.0);
+			glRotatef((GLfloat)alpha+(i*10), 0.0, 0.0, 1.0);
+			glTranslatef(1, 0.0, 0.0);
+			glPushMatrix();
+			glScalef(2, 0.4, 1.0);
+			glutSolidCube(1.0);
+			glPopMatrix();
+		}
+		
+	glPopMatrix();
 }
